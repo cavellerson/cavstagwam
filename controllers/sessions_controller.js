14b5b2e69966b21489aa3b4e350084304aafc8fc
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-03-29 07:03:55 -0400

    trying sesssions/auth

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -3,15 +3,48 @@ const express = require('express')
 const sessions = express.Router()
 const pool = require("../db.js")
 
-sessions.get('/new', (req, res) => {
-    res.send(req.session.currentUser)
+sessions.get('/new', async(req, res) => {
+    try {
+        const foundUser = await pool.query(
+            `SELECT username FROM usernames WHERE username='${req.body.username}';`
+        )
+        console.log(foundUser.rows[0]["username"]);
+        res.send(req.session.currentUser)
+    } catch (err) {
+        console.error(err.message);
+    }
+
 })
 
-// sessions.post('/', async(req, res) => {
-//     try {
-//         const foundUser = await pool.query(
-//             "SELECT * FROM usernames WHERE password=($1)", [req.body.username]
-//         )
-//         const foundPassword = await pool.query
-//     }
-// })
+sessions.post('/', async(req, res) => {
+    const queryData = await pool.query(
+        `SELECT * FROM usernames WHERE username='${req.body.username}'`
+    )
+    console.log(`QueryData: ${JSON.stringify(queryData["rows"])}`);
+    console.log("current user:",req.session);
+
+    const queryUserData = await pool.query(
+        `SELECT username FROM usernames WHERE username='${req.body.username}'`
+    )
+    const foundUsername = queryUserData["rows"][0]["username"]
+    console.log(`found username: ${foundUsername}`);
+
+    const queryPasswordData = await pool.query(
+        `SELECT password FROM usernames WHERE username='${req.body.username}'`
+    )
+    // console.log(`password data: ${queryPasswordData}`);
+    // console.log(queryPasswordData);
+    const foundPassword = queryPasswordData["rows"][0]["password"]
+    // console.log(foundPassword);
+    if (!foundUsername) {
+        res.send('Sorry user not found')
+    }
+    if (bcrypt.compareSync(req.body.password, foundPassword)) {
+        console.log("passwords match");
+        req.session.currentUser = [foundUsername, foundPassword]
+    }
+})
+
+
+
+module.exports = sessions;
--- server.js
+++ server.js
@@ -3,9 +3,22 @@ const app = express();
 const PORT = 3000;
 const cors = require("cors");
 const pool = require("./db")
-
+const session = require('express-session')
+// const cookieParser = require("cookieParser")
+//
+// require('dotenv').config()
 app.use(cors());
 app.use(express.json()); // allows access to req.body
+// app.use(
+// 	session({
+// 		secret: process.env.SECRET,
+// 		resave: false,
+// 		saveUninitialized: false,
+// 	})
+// )
+
+// app.use(express.cookieParser(process.env.SECRET));
+// app.use(express.session());
 
 const postsController = require('./controllers/posts_controller.js')
 app.use('/posts', postsController)
@@ -13,6 +26,8 @@ app.use('/posts', postsController)
 const usersController = require('./controllers/users_controller.js')
 app.use('/users', usersController)
 
+const sessionsController = require('./controllers/sessions_controller.js')
+app.use('/sessions', sessionsController)
 
 
 app.listen(PORT, () => {
