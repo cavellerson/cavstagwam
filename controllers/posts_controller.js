f46e668ea5116b5c2267b366607fbbde9b31511e
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-04-16 15:09:43 -0400

    users can now edit their posts on their Profile page

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -15,7 +15,7 @@ posts.use(isAuthenticated)
 
 //create a posts
 posts.get('/create', isAuthenticated, (req, res) => {
-    console.log(req.body.username);
+    // console.log(req.body.username);
     res.render('createPost.ejs',
     {
         username: req.session.currentUser[0]
@@ -28,7 +28,7 @@ posts.post('/create', isAuthenticated, async(req, res) => {
         let username = req.session.currentUser[0]
         let likes = 0
         let { image } = req.body
-        console.log(`req.body: ${JSON.stringify(req.body)}`);
+        // console.log(`req.body: ${JSON.stringify(req.body)}`);
         const newPost = await pool.query(
             "INSERT INTO posts (description, username, likes, image) VALUES($1,$2,$3,$4) RETURNING *", [description, username, likes, image]
         )
@@ -67,10 +67,14 @@ posts.get(`/:username`, isAuthenticated, async(req, res) => {
         const queryData = await pool.query("SELECT * FROM posts WHERE username = $1",[req.params.username])
 
         let allPosts = queryData["rows"]
-        console.log(allPosts);
+        // console.log(allPosts);
+        // console.log(req.session.currentUser[0]);
+
         res.render('userProfile.ejs', {
             username: req.params.username,
-            allPosts
+            allPosts,
+            currentUser: req.session.currentUser[0],
+
         })
     } catch (err) {
         console.error(err.message)
@@ -94,13 +98,36 @@ posts.get('/:username/:id', isAuthenticated, async(req, res) => {
     }
 })
 
-// update a post
+//shows the page to edit the post
+posts.get('/:username/:id/edit', isAuthenticated, async(req, res) => {
+    try {
+        const postID = req.params.id
+        const queryData = await pool.query("SELECT * FROM posts WHERE post_id = $1", [postID])
+        // console.log(req.session.currentUser);
+        // console.log(queryData);
+        const post = queryData["rows"][0]
+        // console.log(post);
+        if (req.params.username === req.session.currentUser[0]) {
+            res.render('editPost.ejs',{
+                post: post
+            })
+        } else {
+            res.send("<h1> you don't have permission to update</h1>")
+        }
+
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// update/edit a post
 posts.put('/:id', isAuthenticated, async(req, res) => {
     try {
-        const { id } = req.params
-        const { description } = req.body
+        const id = req.params.id
+        const description = req.body.description
         const updatePost = await pool.query("UPDATE posts SET description = $1 WHERE post_id = $2", [description, id])
-        res.json("Post was updated")
+        console.log("post was updated");
+        res.redirect('/posts')
     }
     catch (err) {
         console.error(message)
